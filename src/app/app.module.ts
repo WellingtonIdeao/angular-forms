e3ecbd09dfa46f3807b9f93cc58cacfb2aa1eb27
Wellington Ideão <wellington.ideao@gmail.com>
2022-07-29 21:25:10 -0300

    Imported FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TemplateFormsComponent } from './template-forms/template-forms.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
